84e7125f0957c5d3c2fbc303946ca78ad8c8bf8a
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2021-09-28 17:00:06 -0300

    metodo de listar extrado adicionado

--- src/index.js
+++ src/index.js
@@ -18,16 +18,22 @@ app.post("/account", (request, response) => {
         return response.status(400).json({ error: "Cliente já existe!" });
     }
 
-    const id = uuidv4();
-
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     });
 
     return response.status(201).send();
+});
+
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    return response.json(customer.statement);
 });
 
 app.listen(8001)
